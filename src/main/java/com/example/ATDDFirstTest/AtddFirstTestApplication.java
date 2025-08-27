4bd11725d5dbb8378889f9270988ba7007d2f5bd
L0G1C06 <justgithub70@gmail.com>
2025-08-26 21:58:59 -0300

    first commit

---
+++ src/main/java/com/example/ATDDFirstTest/AtddFirstTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ATDDFirstTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddFirstTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtddFirstTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ATDDFirstTest/AtddFirstTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ATDDFirstTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtddFirstTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
